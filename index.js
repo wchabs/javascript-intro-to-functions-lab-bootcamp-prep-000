85777ffc87f31067c2a37c8b332f31a1b5479202
William Chabla <w_chabla@hotmail.com>
2018-03-07 03:47:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function shout(string) {
   return string.toUpperCase();
 }
+
+function whisper(string) {
+  return sting.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
